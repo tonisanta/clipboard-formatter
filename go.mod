044bdbf352f81a964d0b61315eba04be754fb67f
Antoni Santandreu Sureda <santandreutoni@gmail.com>
2022-05-21 18:16:37 +0200

    First commit!

---
+++ go.mod
@@ -0,0 +1,15 @@
+module clipboard-formatter
+
+go 1.18
+
+require (
+	github.com/go-xmlfmt/xmlfmt v0.0.0-20220206211657-0a94163c4677
+	golang.design/x/clipboard v0.6.2
+)
+
+require (
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"github.com/go-xmlfmt/xmlfmt"
+	"golang.design/x/clipboard"
+	"log"
+	"sync"
+	"time"
+)
+
+const identTwoSpaces = "  "
+
+func main() {
+	err := clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Listening ...")
+
+	ch := clipboard.Watch(context.Background(), clipboard.FmtText)
+	for data := range ch {
+		log.Println("new data to process")
+		var wg sync.WaitGroup // number of working goroutines
+		wg.Add(2)
+		start := time.Now()
+		go processJson(data, ch, &wg)
+		go processXml(data, ch, &wg)
+		wg.Wait()
+		elapsedTime := time.Since(start)
+		log.Printf("execution time %s\n", elapsedTime)
+	}
+}
+
+func processJson(data []byte, ch <-chan []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	buffer, err := formatJson(data)
+	if err != nil {
+		log.Default().Println("Error while checking JSON: ", err)
+		return
+	}
+	clipboard.Write(clipboard.FmtText, buffer.Bytes())
+	<-ch // we want to ignore the message that has just been written, as it's the formatted code
+}
+
+func formatJson(jsonBytes []byte) (bytes.Buffer, error) {
+	var out bytes.Buffer
+	if !json.Valid(jsonBytes) {
+		return out, fmt.Errorf("is not JSON")
+	}
+	err := json.Indent(&out, jsonBytes, "", identTwoSpaces)
+	if err != nil {
+		return out, err
+	}
+	return out, nil
+}
+
+func processXml(data []byte, ch <-chan []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	formattedXml, err := formatXml(data)
+	if err != nil {
+		log.Default().Println("Error while checking XML: ", err)
+		return
+	}
+	clipboard.Write(clipboard.FmtText, []byte(formattedXml))
+	<-ch // we want to ignore the message that has just been written, as it's the formatted code
+}
+
+func formatXml(data []byte) (string, error) {
+	if !isValidXml(data) {
+		return "", fmt.Errorf("is not XML")
+	}
+	formattedXml := xmlfmt.FormatXML(string(data), "", identTwoSpaces)
+	return formattedXml, nil
+}
+
+func isValidXml(data []byte) bool {
+	return xml.Unmarshal(data, new(interface{})) == nil
+}
